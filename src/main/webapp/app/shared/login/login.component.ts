1e736f88f708f0e81233753b57bf0f53c89eb464
Preet <preet.kaur1093@gmail.com>
2018-11-27 17:45:14 +0100

    Customize the style and layout of the webapp

--- src/main/webapp/app/shared/login/login.component.ts
+++ src/main/webapp/app/shared/login/login.component.ts
@@ -64,10 +64,11 @@ export class JhiLoginModalComponent implements AfterViewInit {
 
                 // previousState was set in the authExpiredInterceptor before being redirected to login modal.
                 // since login is succesful, go to stored previousState and clear previousState
-                const redirect = this.stateStorageService.getUrl();
+                const redirect = true;
                 if (redirect) {
+                    this.stateStorageService.getUrl();
                     this.stateStorageService.storeUrl(null);
-                    this.router.navigate([redirect]);
+                    this.router.navigate(['/submissions']);
                 }
             })
             .catch(() => {
