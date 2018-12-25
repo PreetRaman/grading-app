f0cf7ba13f69134c508f9b17483ef42b267ca852
Preet <preet.kaur1093@gmail.com>
2018-12-25 14:15:56 +0100

    Add fdai number resource

--- src/main/webapp/app/entities/entity.module.ts
+++ src/main/webapp/app/entities/entity.module.ts
@@ -1,12 +1,14 @@
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 import { GradingAppSubmissionsModule } from './submissions/submissions.module';
+import { GradingAppFdaiNummerModule } from './fdai-nummer/fdai-nummer.module';
 /* jhipster-needle-add-entity-module-import - JHipster will add entity modules imports here */
 
 @NgModule({
     // prettier-ignore
     imports: [
         GradingAppSubmissionsModule,
+        GradingAppFdaiNummerModule,
         /* jhipster-needle-add-entity-module - JHipster will add entity modules here */
     ],
     declarations: [],
